8d561e549d834223acae46e69b3a33b8700f9900
Aki Van Ness <aki@lethalbit.net>
2022-07-04 04:30:12 -0400

    libclank: lex: added some initial Unicode tables for valid character ranges

---
+++ src/libclank/internal/unicode_range.hh
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+#if !defined(libclank_internal_unicode_range_hh)
+#define libclank_internal_unicode_range_hh
+
+#include <cstdint>
+
+namespace libclank::internal {
+	struct UnicodeRange_t final {
+	private:
+		std::uint32_t _low;
+		std::uint32_t _high;
+	public:
+
+		constexpr UnicodeRange_t(std::uint32_t low, std::uint32_t high) noexcept :
+			_low{low}, _high{high} { /* */ }
+
+		inline bool operator<(std::uint32_t v) {
+			return v < _low;
+		}
+
+		inline bool operator>(std::uint32_t v) {
+			return v > _high;
+		}
+	};
+}
+
+#endif /* libclank_internal_unicode_range_hh */
