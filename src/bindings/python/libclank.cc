55e82ce1ef4bd6b6b0f092ae852491eae767fb16
Aki Van Ness <aki@lethalbit.net>
2022-07-04 03:27:42 -0400

    libclank: Fixed the namespace capitialization

--- src/bindings/python/libclank.cc
+++ src/bindings/python/libclank.cc
@@ -9,6 +9,6 @@ namespace py = pybind11;
 PYBIND11_MODULE(libclank, m) {
 
 	m.doc() = "A C++ Front-end based on LLVM's clang";
-	m.attr("__version__") = libclank::Config::version;
+	m.attr("__version__") = libclank::config::version;
 
 }
