251025273377f47944a201a7436962a1f353c485
Aki Van Ness <aki@lethalbit.net>
2022-07-22 23:23:33 -0400

    examples: added a small example

---
+++ examples/simple_parser.cc
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Apache-2.0
+/* simple_parser.cc - Simple clank example */
+
+#include <iostream>
+#include <string_view>
+
+#include <libclank/clank.hh>
+
+using namespace std::literals::string_view_literals;
+
+using clank::core::LanguageID_t;
+using clank::core::LanguageStandard_t;
+
+int main(int, char**) {
+	/* Some simple C to parse */
+	const auto src{"int a = 1;"sv};
+
+	if (auto lang = LanguageStandard_t::get_by_id(LanguageID_t::C11)) {
+		clank::parse(src, *lang).map([](const auto& ast){
+			/* Do AST things */
+		}).map_error([](const std::error_code err){
+			std::cerr << "Unable to get AST: " << err.message() << "\n";
+		});
+	} else {
+		std::cerr << "Unable to get language standard for 'C11'\n";
+		return 1;
+	}
+
+	return 0;
+}
